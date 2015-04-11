665624a92d3d89af3a3cb4c45e6ccd51d7905b88
AndreyBulgakov <pivolit@gmail.com>
2015-04-11 23:44:12 +0300

    Fix commit. Now formats are realy pretty.

--- PrinterCombnators/Pretty.fs
+++ PrinterCombnators/Pretty.fs
@@ -1,5 +1,5 @@
 ﻿module Pretty
-
+//Рассмотреть вариант,что адд и бесайл не нуждаются в чек апдейте и все делать в чойсе. Перевести в форматы
 open Doc
 open Format
 open System.Collections.Generic
@@ -13,11 +13,20 @@ let inline update (form : Format) (map1 : Dictionary<Frame, Format>) =
 
 ///Insert format to map if format is suitable. If map empty then insert non-suitable format.
 let checkUpdate wid (form : Format) (map1 : Dictionary<Frame, Format>) = 
-    if map1.Count = 0 || form.isSuitable wid
+    let mutable min = map1.First()
+    if form.isSuitable wid
     then update form map1
    
 let mIter f d1 (d2:Dictionary<_,_>) =
     for kvp in d2 do f kvp.Value d1
+ 
+let sortUpdate wid (result : Dictionary<Frame,Format>) (d2:Dictionary<Frame,Format>) =
+    let mutable elem = d2.Values.First()
+    for x in d2.Values do
+        if x.isSuitable wid
+        then update x result
+        elif x.totalW < elem.totalW then elem <- x
+    update elem result
 
 ///Insert in in map1 elements from map2
 let mapmerge (map1 : Dictionary<Frame, Format>) (map2 : Dictionary<Frame, Format>) =    
@@ -30,28 +39,27 @@ let rec docToFormats wid doc =
     | Text(str) -> update (stringToFormat str) res
     | Indent(ind, indDoc) -> 
         let l = docToFormats (wid - ind) indDoc        
-        mIter (fun y acc -> checkUpdate wid (indentFormat ind y) acc) res l
+        mIter (fun y acc -> update (indentFormat ind y) acc) res l
     | Above(doc1, doc2) -> 
         let dlist1 = docToFormats wid doc1
         let dlist2 = docToFormats wid doc2
-        let aboveForL (l : Format) = mIter (fun r acc -> (checkUpdate wid)(l >-< r) acc) res dlist2        
+        let aboveForL (l : Format) = mIter (fun r acc -> update (l >-< r) acc) res dlist2        
         for kvp in dlist1 do aboveForL kvp.Value
     | Beside(doc1, doc2) -> 
         let dlist1 = docToFormats wid doc1
         let dlist2 = docToFormats wid doc2
-        let besideForL (l : Format) = mIter (fun r acc -> checkUpdate wid (l >|< r) acc) res dlist2
+        let besideForL (l : Format) = mIter (fun r acc -> update (l >|< r) acc) res dlist2
         for kvp in dlist1 do besideForL kvp.Value
     |Fill(doc1, shift, doc2) ->
         let dlist1 = docToFormats wid doc1
         let dlist2 = docToFormats wid doc2
-        let fillForL (l : Format) = mIter (fun r acc -> checkUpdate wid (Format.addFill(l,r,shift)) acc) res dlist2
+        let fillForL (l : Format) = mIter (fun r acc -> update (Format.addFill(l,r,shift)) acc) res dlist2
         for kvp in dlist1 do fillForL kvp.Value
     | Choice(doc1, doc2) -> 
         let dlist1 = docToFormats wid doc1
         let dlist2 = docToFormats wid doc2        
         mapmerge dlist1 dlist2
-        //mapmerge res dlist1
-        mIter (checkUpdate wid) res dlist1
+        sortUpdate wid res dlist1
     res
 
 ///Get pretty format         
@@ -63,13 +71,12 @@ let pretty (resultWidth : int) (d : Doc) =
 
 ///Get suitable and min
 let best wid (map1:Dictionary<Frame, Format>) =
-    let mutable min = map1.Values.First()//map1.GetEnumerator().Current.Value
-    //let fd = map1.Values.GetEnumerator().Current
+    let mutable value = map1.Values.First()
     for x in map1.Values do
-        //(x.isSuitable wid && x.height < min.height) || (not (min.isSuitable wid) && x.totalW < min.totalW)
-        if x.isSuitable wid || x.height < min.height
-        then min <- x
-    min
+        let suit = x.isSuitable wid
+        if (suit && x.height < value.height) || (not suit && x.totalW < value.totalW)
+        then value <- x
+    value
     
 ///Get pretty format with Best       
 let prettyPrints (resultWidth : int) (d : Doc) = 
